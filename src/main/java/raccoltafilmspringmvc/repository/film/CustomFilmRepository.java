e9f69dc20109c09c09bc88d96e89c7cb0d2f9054
Tommy2001-web <tommy.lenzi@gmail.com>
2026-03-20 11:21:19 +0100

    creazione model e dto

--- src/main/java/raccoltafilmspringmvc/repository/film/CustomFilmRepository.java
+++
@@ -1,9 +0,0 @@
-package raccoltafilmspringmvc.repository.film;
-
-import it.prova.raccoltafilmspringmvc.model.Film;
-
-import java.util.List;
-
-public interface CustomFilmRepository {
-    List<Film> findByExample(Film example);
-}
--- src/main/java/raccoltafilmspringmvc/repository/regista/CustomRegistaRepository.java
+++
@@ -1,9 +0,0 @@
-package raccoltafilmspringmvc.repository.regista;
-
-import it.prova.raccoltafilmspringmvc.model.Regista;
-
-import java.util.List;
-
-public interface CustomRegistaRepository {
-	List<Regista> findByExample(Regista example);
-}
